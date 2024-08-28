c38ba68c51166ecf26b15dbf7dd80410d2759d63
Ana Nunes <ananunes.dev@gmail.com>
2024-08-28 20:15:40 -0300

    create CrudBiblioteca
    package e class, e métodos dentro do crud

---
+++ SistemaBiblioteca/src/br/com/sistemabiblioteca/crud/CrudBiblioteca.java
@@ -0,0 +1,16 @@
+package br.com.sistemabiblioteca.crud;
+
+public class CrudBiblioteca {
+	//method to create a new book
+	public static void create() {
+		System.out.println("CREATE");
+	}
+	
+	//method to read all the books
+	public static void read() {
+		System.out.println("READ");
+	}
+	
+	
+	
+}
